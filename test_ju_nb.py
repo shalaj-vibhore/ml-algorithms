03058a9aac5ffc2aa4e0963a45c55cd9733cfb4c
shalaj vibhore <shalaj.vibhore@gmail.com>
2018-06-07 15:16:35 +0530

    added initial version of travis for test

---
+++ test_ju_nb.py
@@ -0,0 +1,15 @@
+import subprocess
+import tempfile
+
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+        print("Test Complete !")
+
+
+
+_exec_notebook('./basic-ml-algo/classification-algo/LogisticRegression.ipynb')
